f58e38c5e62f9ac2baeb46df6240252c4323ee47
FadliAchsan <79966300+FadliAchsan@users.noreply.github.com>
2023-10-02 13:33:58 +0700

    Update matematika_a.go

--- matematika_a.go
+++ matematika_a.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var x int
-	var y int
-
-	fmt.Print("Tolong masukan nilai x dan y:")
-	fmt.Scan(&x, &y)
-
-	var fx = (1 / ((3 * x * x) + 10)) + ((10 * y) + 7)
-	fmt.Println(fx) // hasil sudah dibulatkan
+	var x, y, hasil float64
+	fmt.Scanln(&x, &y)
+	hasil = 1 / ( 3 * ( x * x ) + 10 ) + 10 * y + 7
+	fmt.Println(hasil)
 }
