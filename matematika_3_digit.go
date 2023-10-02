24d51d2de700bdc4fd2522ffa55540c98f1347d7
FadliAchsan <79966300+FadliAchsan@users.noreply.github.com>
2023-10-02 13:24:38 +0700

    Update matematika_3_digit.go

--- matematika_3_digit.go
+++ matematika_3_digit.go
@@ -3,21 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var x int
+	var x, v1, v2, v3, hb int32
+	fmt.Scanln(&x)
+	v1 = x / 100
+	hb = x % 100
+	v2 = hb / 10
+	v3 = hb % 10
 
-	fmt.Print("Masukkan bilangan bulat positif (<= 999): ")
-	fmt.Scan(&x)
-
-	if x <= 0 || x > 999 {
-		fmt.Println("Masukkan bilangan bulat positif kurang dari 999 yang valid.")
-		return
-	} else {
-		d3 := x % 10
-		d2 := (x / 10) % 10
-		d1 := x % 10
-
-		fmt.Printf("Digit pertama: %d\n", d1)
-		fmt.Printf("Digit kedua: %d\n", d2)
-		fmt.Printf("Digit ketiga: %d\n", d3)
-	}
+	fmt.Println(v1, v2, v3)
 }
